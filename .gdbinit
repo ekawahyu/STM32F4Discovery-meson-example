806c8a8300a3a9dce42ce6a37a3b2a56ca3e7e26
Ekawahyu Susilo <ekawahyu@icloud.com>
2021-12-02 00:36:43 -0800

    updated submodules and ported F3 to F4 meson build files

---
+++ .gdbinit
@@ -0,0 +1,6 @@
+target remote:3333
+layout src
+load
+b main
+monitor reset halt
+continue
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stm32f4xx_hal.h"
+
+#define LED_PIN                                GPIO_PIN_12
+#define LED_GPIO_PORT                          GPIOD
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOD_CLK_ENABLE()
+
+#endif // MAIN_H
